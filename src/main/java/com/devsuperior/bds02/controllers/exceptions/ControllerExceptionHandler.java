77cbc2912a493230b0c052253fd031dcad3e4e5f
VanssaJaniszewski <vjaniszewski@gmail.com>
2022-12-27 16:28:34 -0300

    auto tests for unit 2

---
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.devsuperior.bds02.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exceptions.DataBaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+		
+		@ExceptionHandler(ResourceNotFoundException.class)
+		public ResponseEntity<MyStandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+			MyStandardError err = new MyStandardError();
+			err.setTimestamp(Instant.now());
+			err.setStatus(HttpStatus.NOT_FOUND.value());
+			err.setError("Resource not found.");
+			err.setMessage(e.getMessage());
+			err.setPath(request.getRequestURI());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		};
+		
+		@ExceptionHandler(DataBaseException.class)
+		public ResponseEntity<MyStandardError> integrityViolation(DataBaseException e, HttpServletRequest request){
+			MyStandardError err = new MyStandardError();
+			err.setTimestamp(Instant.now());
+			err.setStatus(HttpStatus.BAD_REQUEST.value());
+			err.setError("Database exception.");
+			err.setMessage(e.getMessage());
+			err.setPath(request.getRequestURI());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		};
+}
